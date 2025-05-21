cf28ce1f1f2c0e8e1f88916cd4fead649749a64d
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-05-21 21:53:50 +0300

    feat: create an entry point for the app

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import Print from './print'
 import './styles/reset.css'
 import './styles/style.css'
 
-
 const a = 555
-
-
-
 
 function component() {
   const element = document.createElement('h1')
 
   element.innerHTML = 'Simple Calculator'
-  element.classList.add("box");
+  element.classList.add('box')
 
   element.onclick = Print.bind(null, 'simple calculator')
 
