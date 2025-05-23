89a3794f9efcc3f1d06c56a95f038cf08612a950
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-05-23 13:37:47 +0300

    fix: issue with large numbers

--- src/features/numberOperations/constructFloatNumber.js
+++ src/features/numberOperations/constructFloatNumber.js
@@ -3,7 +3,7 @@ import handleCurrentValue from '../handleCurrentValue.js'
 export default function constructFloatNumber() {
   const { currentValue, update } = handleCurrentValue()
 
-  if (/^-?\d+$/.test(currentValue)) {
+  if (/^-?\d+$/.test(currentValue) && currentValue.length < 9) {
     update(`${currentValue}.`)
   }
 }
--- src/features/numberOperations/constructNumber.js
+++ src/features/numberOperations/constructNumber.js
@@ -4,6 +4,10 @@ export default function constructNumber(numb) {
   const { currentValue, update } = handleCurrentValue()
   let newValue = ''
 
+  if(currentValue.length === 10) {
+    return
+  }
+
   if (/^0$/.test(currentValue)) {
     newValue = numb
   } else if (/^-0$/.test(currentValue)) {
