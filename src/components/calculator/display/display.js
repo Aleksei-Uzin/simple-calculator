d827fc8a7e5208be4b7fa1724b05968ac08dedf2
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-05-23 17:17:50 +0300

    feat: update display component

--- src/components/calculator/display/display.js
+++ src/components/calculator/display/display.js
@@ -1,13 +1,16 @@
 import createElement from 'utils/createElement'
+import handleLocalStorage from 'utils/handleLocalStorage'
 
 export const initValue = '0'
 
+const ROW_1_PARAMS = { className: 'display-second-row', id: 'display-second-row' }
+
+const ROW_2_PARAMS = { className: 'display-main-row', id: 'display-main-row', 'data-value': initValue }
+
 export function getDisplay() {
-  const row1 = createElement('div', { className: 'display-second-row', id: 'display-second-row' })
-  const row2 = createElement(
-    'div',
-    { className: 'display-main-row', id: 'display-main-row', 'data-value': initValue },
-    initValue,
-  )
+  const { getStorage } = handleLocalStorage()
+  const row1 = createElement('div', ROW_1_PARAMS, getStorage().join(''))
+  const row2 = createElement('div', ROW_2_PARAMS, initValue)
+
   return createElement('div', { className: 'display' }, row1, row2)
 }
