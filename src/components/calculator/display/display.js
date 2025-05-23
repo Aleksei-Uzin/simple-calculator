9a600109f6e74371a330b39206e1bce6e5cb51a3
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-05-23 10:33:39 +0300

    feat: update display component

--- src/components/calculator/display/display.js
+++ src/components/calculator/display/display.js
@@ -1,5 +1,7 @@
 import createElement from 'utils/createElement'
 
+export const initValue = '0'
+
 export function getDisplay() {
-  return createElement('div', { className: 'display' })
+  return createElement('div', { className: 'display', id: 'display', 'data-value': initValue }, initValue)
 }
